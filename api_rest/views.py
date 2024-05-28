1b80a0bad5ea2f13d3ef80f48f973d13fc4868a8
Leonardo Heringer <69641220+LeoHeringer@users.noreply.github.com>
2024-05-28 01:52:02 -0300

    Update views.py

--- api_rest/views.py
+++ api_rest/views.py
@@ -13,8 +13,6 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 
 import json
 
-
-
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 @renderer_classes([XMLRenderer])
@@ -26,7 +24,6 @@ def get_users_xml(request):
     
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_users_json(request):
@@ -37,15 +34,11 @@ def get_users_json(request):
     
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-    
-
 @api_view(['GET'])
 @renderer_classes([XMLRenderer])
 @permission_classes([IsAuthenticated])
 def get_by_nick_xml(request):
 
-
     if not request.GET['user']:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -58,13 +51,11 @@ def get_by_nick_xml(request):
     
     serializer = UserSerializer(user.first())
     return Response(serializer.data, status=status.HTTP_200_OK)  
-
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_by_nick_json(request):
 
-
     if not request.GET['user']:
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
@@ -78,7 +69,6 @@ def get_by_nick_json(request):
     serializer = UserSerializer(user.first())
     return Response(serializer.data, status=status.HTTP_200_OK)  
 
-    
 @api_view(['POST'])
 @renderer_classes([XMLRenderer])
 @permission_classes([IsAuthenticated])
@@ -107,7 +97,6 @@ def create_user_json(request):
         return Response({'message':'created'}, status=status.HTTP_201_CREATED)
 
     return Response({'message':'invalid'}, status=status.HTTP_400_BAD_REQUEST)
-    
 
 @api_view(['PUT'])
 @renderer_classes([XMLRenderer])
@@ -117,7 +106,7 @@ def update_user_xml(request):
     cliente_id = request.query_params.get('id-usuario')
 
     if not cliente_id: 
-        return Response({'message': 'dado incorreto'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'incorrect data'}, status=status.HTTP_400_BAD_REQUEST)
 
     update_user = User.objects.filter(id=cliente_id).first()
     
@@ -127,17 +116,15 @@ def update_user_xml(request):
     is_superuser = request.data.get('is_superuser')
 
     if is_superuser is None:
-        return Response({'message': 'dados não fornecidos'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'data not provided'}, status=status.HTTP_400_BAD_REQUEST)
     
     if not isinstance(is_superuser, bool):
-        return Response({'message': 'dado incorreto'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'incorrect data'}, status=status.HTTP_400_BAD_REQUEST)
     
     update_user.is_superuser = is_superuser
     update_user.save()
 
-    
-    return Response({'mensagem': 'Usuário atualizado com sucesso'}, status=status.HTTP_200_OK)
- 
+    return Response({'message': 'User updated successfully'}, status=status.HTTP_200_OK)
 
 @api_view(['PUT'])
 @permission_classes([IsAuthenticated])
@@ -146,7 +133,7 @@ def update_user_json(request):
     cliente_id = request.query_params.get('id-usuario')
 
     if not cliente_id: 
-        return Response({'message': 'dado incorreto'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'incorrect data'}, status=status.HTTP_400_BAD_REQUEST)
 
     update_user = User.objects.filter(id=cliente_id).first()
     
@@ -156,18 +143,16 @@ def update_user_json(request):
     is_superuser = request.data.get('is_superuser')
 
     if is_superuser is None:
-        return Response({'message': 'dados não fornecidos'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'data not provided'}, status=status.HTTP_400_BAD_REQUEST)
     
     if not isinstance(is_superuser, bool):
-        return Response({'message': 'dado incorreto'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'message': 'incorrect data'}, status=status.HTTP_400_BAD_REQUEST)
     
     update_user.is_superuser = is_superuser
     update_user.save()
 
-    
-    return Response({'mensagem': 'Usuário atualizado com sucesso'}, status=status.HTTP_200_OK)
+    return Response({'message': 'User updated successfully'}, status=status.HTTP_200_OK)
  
-    
 @api_view(['DELETE'])
 @renderer_classes([XMLRenderer])
 @permission_classes([IsAuthenticated])
@@ -179,10 +164,9 @@ def delete_user_xml(request):
 
     if user_to_delete:
         user_to_delete.delete()
-        return Response({'mensagem': 'Usuário deletado com sucesso'}, status=status.HTTP_200_OK)
+        return Response({'message': 'User deleted successfully'}, status=status.HTTP_200_OK)
 
-    return Response({'message': 'Usuário não encontrado'}, status=status.HTTP_404_NOT_FOUND)
-
+    return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['DELETE'])
 @permission_classes([IsAuthenticated])
@@ -194,9 +178,6 @@ def delete_user_json(request):
 
     if user_to_delete:
         user_to_delete.delete()
-        return Response({'mensagem': 'Usuário deletado com sucesso'}, status=status.HTTP_200_OK)
+        return Response({'message': 'User deleted successfully'}, status=status.HTTP_200_OK)
 
-    return Response({'message': 'Usuário não encontrado'}, status=status.HTTP_404_NOT_FOUND)
-
-
-
+    return Response({'message': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
